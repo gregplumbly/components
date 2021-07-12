212c3d2fde26b0e288b711ea27f4289cf3eeb48f
Greg Plumbly <greg.plumbly@gmail.com>
2021-07-13 00:12:18 +0100

    approval card

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>Are you sure you want to do this?</ApprovalCard>
       <ApprovalCard>
         <CommentDetail
           author="Sam"
