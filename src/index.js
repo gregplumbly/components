dd7819e9878620f1ee5788f1afd4e6c96e60a853
Greg Plumbly <greg.plumbly@gmail.com>
2021-07-12 23:58:41 +0100

    component reuse

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDom from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
-    <div className='ui container comments'>
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4pm'
-        comment='nice blah blah'
-      />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 3pm'
-        comment='interesting'
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Last week'
-        comment='blah blah blah'
-      />
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4pm"
+          comment="nice blah blah"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 3pm"
+          comment="interesting"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Last week"
+          comment="blah blah blah"
+        />
+      </ApprovalCard>
     </div>
   );
 };
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(<App />, document.querySelector("#root"));
